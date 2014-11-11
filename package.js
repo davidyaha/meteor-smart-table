dd946ef03f36bec16209ebb01096ff1dc7f4e3e0
David Yahalomi <davidyaha@gmail.com>
2014-11-11 20:08:48 +0200

    Added a new plugin made by myself: stFilterSelect ;)

--- package.js
+++ package.js
@@ -1,6 +1,6 @@
 Package.describe({
   summary: "Meteor package for smart-table: a table/grid for Angularjs",
-  version: "0.0.2",
+  version: "0.1.0",
   git: "https://github.com/davidyaha/meteor-smart-table.git"
 });
 
